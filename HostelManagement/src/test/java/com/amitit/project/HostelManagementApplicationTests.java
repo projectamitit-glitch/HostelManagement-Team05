934137b8b7cfb23eb9550761f241a04890bc05c6
maheshpande2015@gmail.com <Mahesh@00998877>
2025-11-22 13:44:41 +0530

    initial commit

---
+++ HostelManagement/src/test/java/com/amitit/project/HostelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amitit.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
