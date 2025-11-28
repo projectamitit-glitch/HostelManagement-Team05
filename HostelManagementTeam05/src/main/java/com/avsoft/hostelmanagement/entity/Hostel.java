ac96536802b00582ad8fa554163c140f421b72e5
shruti bhole <bholeshruti9@gmail.com>
2025-11-28 18:42:25 +0530

    i am create hostel api which is test on local system and it is good

--- HostelManagementTeam05/src/main/java/com/avsoft/hostelmanagement/entity/Hostel.java
+++ HostelManagementTeam05/src/main/java/com/avsoft/hostelmanagement/entity/Hostel.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
