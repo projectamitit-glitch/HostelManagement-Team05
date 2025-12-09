28f6ea04ee8ce078d865f589052da606d87ed95d
Akash D. Balkunde <104627797+BalkundeAkash@users.noreply.github.com>
2025-12-09 19:47:11 +0530

    Delete HostelManagementTeam05 directory

--- HostelManagementTeam05/src/main/java/com/avsoft/hostelmanagement/repostiory/BuildingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.avsoft.hostelmanagement.repostiory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.avsoft.hostelmanagement.entity.Building;
-
-public interface BuildingRepository extends JpaRepository<Building, Long> {
-    
-}
