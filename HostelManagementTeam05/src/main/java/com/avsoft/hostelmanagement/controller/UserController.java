299982eef7cb6d598b39ad42cef7fa9aa6e58f4f
Mahesh <maheshpande2015@gmail.com>
2025-12-09 09:55:17 +0530

    Do not change anything this push is totally for jenking supported push

--- HostelManagementTeam05/src/main/java/com/avsoft/hostelmanagement/controller/UserController.java
+++ HostelManagementTeam05/src/main/java/com/avsoft/hostelmanagement/controller/UserController.java
@@ -1,23 +1,16 @@
 package com.avsoft.hostelmanagement.controller;
 
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.avsoft.hostelmanagement.util.MessageConstant;
-import com.avsoft.hostelmanagement.entity.User;
-import com.avsoft.hostelmanagement.response.ApiResponse;
-import com.avsoft.hostelmanagement.service.UserService;
-
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import com.avsoft.hostelmanagement.dto.OtpVerificationDto;
 import com.avsoft.hostelmanagement.dto.UserSignupDto;
