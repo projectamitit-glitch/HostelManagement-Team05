30baeeb362be50276226feb70144585e25174b9f
Mahesh Pande <maheshpande2015@gmail.com>
2025-12-09 22:52:17 +0530

    Delete HostelManagement-Team05-main/HostelManagementTeam05 directory

--- HostelManagement-Team05-main/HostelManagementTeam05/src/main/java/com/avsoft/hostelmanagement/repostiory/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.avsoft.hostelmanagement.repostiory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.avsoft.hostelmanagement.entity.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-
-}
--- HostelManagement-Team05-main/HostelManagementTeam05/src/main/java/com/avsoft/hostelmanagement/repostiory/OrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.avsoft.hostelmanagement.repostiory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.avsoft.hostelmanagement.entity.Organization;
-
-public interface OrganizationRepository extends JpaRepository<Organization, Long> {
-
-}
