7b7ff0ba2fecdaf165a7987fbfba3b64fc173daa
pande <maheshpande90136@gmail.com>
2025-12-17 14:05:45 +0530

    remove comments

--- src/main/java/com/avsoft/hostelmanagement/controller/HomeController.java
+++ src/main/java/com/avsoft/hostelmanagement/controller/HomeController.java
@@ -6,16 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     
-    // Option 1: Serve static index.html
     @GetMapping("/")
     public String index() {
-        // Forward to static index.html
         return "forward:/index.html";
     }
     
-    // Option 2: Serve Thymeleaf template
     @GetMapping("/app")
     public String app() {
-        return "index"; // Uses templates/index.html
+        return "index"; 
     }
 }
